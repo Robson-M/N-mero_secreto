5827402abbe542e7039c2f8fef7b8a584ec5d4b0
Robson Mesquita <Robsonmes11@gmail.com>
2024-09-17 16:07:38 -0300

    Comitado0024

--- img/JS Game_files/app.js
+++ img/JS Game_files/app.js
@@ -10,7 +10,7 @@
 //         break
 //     } else {
 //         if (chute > numeroSecreto) {
-//             alert('O número secreto é menor')
+//             alert('O número secjjreto é menor')
 //         } else {
 //             alert('O número secreto é maior')
 //         }
